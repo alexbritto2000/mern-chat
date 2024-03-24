7ab5020432121971d7aa8a92b6ee3a0a80fc99cf
Alex Britto <alexbritto@dotcod.in>
2024-03-24 14:09:10 +0530

    get users API

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,10 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 
-import authRoutes from "./routes/auth.route.js";
-import messageRoutes from "./routes/message.route.js";
+import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
+import userRoutes from "./routes/user.routes.js";
+
 import connectToMongoDB from "./db/connectToMongoDb.js";
 
 const app = express();
@@ -16,6 +18,7 @@ app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
 /* app.get("/", (req, res) => {
   // root route http://localhost:5000/
   res.send("Hello world");
