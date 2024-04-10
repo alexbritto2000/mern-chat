5a0faed3f5050ccb4d7d420446632d3f1f637d2c
Alex Britto <alexbritto@dotcod.in>
2024-04-10 13:21:57 +0530

    Auth context

--- frontend/src/hooks/useSignup.js
+++ frontend/src/hooks/useSignup.js
@@ -1,8 +1,10 @@
 import { useState } from "react";
 import toast from "react-hot-toast";
+import { useAuthContext } from "../context/AuthContext";
 
 const useSignup = () => {
   const [loading, setLoading] = useState(false);
+  const { authUser, setAuthUser } = useAuthContext();
 
   const signup = async ({
     fullName,
@@ -36,6 +38,15 @@ const useSignup = () => {
 
       const data = await res.json();
       console.log(data);
+      if (data.error) {
+        throw new Error(data.error);
+      }
+
+      //localstorage
+      localStorage.setItem("chat-user", JSON.stringify(data));
+      //context
+      setAuthUser(data);
+
     } catch (error) {
       toast.error(error.message);
     } finally {
