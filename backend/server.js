763852aa3904f9cbb53bacb958daa433144b5ae6
Alex Britto <alexbritto@dotcod.in>
2024-04-21 21:05:23 +0530

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,17 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-
+import path from "path";
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
 
 import connectToMongoDB from "./db/connectToMongoDb.js";
-import { app, server } from './socket/socket.js'
+import { app, server } from "./socket/socket.js";
 
 const PORT = process.env.PORT || 5000;
+
+const __dirname = path.resolve();
 
 dotenv.config();
 
@@ -23,8 +25,12 @@ app.use("/api/users", userRoutes);
   // root route http://localhost:5000/
   res.send("Hello world");
 }); */
+app.use(express.static(path.join(__dirname,"/frontend/dist")));
+app.get("*", (req,res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+})
 
 server.listen(PORT, () => {
   connectToMongoDB();
-  console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`);
 });
