832e1c29f44387149eb0916c7c8393eff90eef5d
Alex Britto <alexbritto@dotcod.in>
2024-04-21 20:48:10 +0530

    conversation message shake and beep sound

--- frontend/src/components/sidebar/Conversation.jsx
+++ frontend/src/components/sidebar/Conversation.jsx
@@ -1,3 +1,4 @@
+import { useSocketContext } from "../../context/SocketContext";
 import useGetConversation from "../../zustand/useConversation";
 
 const Conversation = ({ conversation, lastIdx, emoji }) => {
@@ -5,6 +6,9 @@ const Conversation = ({ conversation, lastIdx, emoji }) => {
     useGetConversation();
 
   const isSelected = selectedConversation?._id === conversation._id;
+  const {onlineUsers} = useSocketContext();
+  const isOnline = onlineUsers.includes(conversation._id);
+
   return (
     <>
       <div
@@ -13,7 +17,7 @@ const Conversation = ({ conversation, lastIdx, emoji }) => {
         }`}
         onClick={() => setSelectedConversation(conversation)}
       >
-        <div className="avatar online">
+        <div className={`avatar ${isOnline ? 'online' : ''}`}>
           <div className="w-14 rounded-full">
             <img src={conversation.profilePic} alt="user avatar" />
           </div>
